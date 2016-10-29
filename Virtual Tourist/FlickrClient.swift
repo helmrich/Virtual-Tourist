c991bf8dd20da442f483dd8457f3a964405e87b6
Tobias Helmrich <me@tobias-helmrich.de>
2016-10-29 15:44:31 +0200

    Change networking code
    Instead of using one function that gets all the image data it's now split up into two functions where one gets all the image URLs (getImageUrls(forLatitude:andLongitude:withRadius:completionHandlerForImageUrls:)) and the other function (downloadImageData(fromUrl:completionHandlerForImageData:)) is used to download one image. This change was made to provide faster feedback on how many images are available and to improve asynchrony.

--- Virtual Tourist/FlickrClient.swift
+++ Virtual Tourist/FlickrClient.swift
@@ -20,7 +20,7 @@ class FlickrClient {
     
     // MARK: - Methods
     
-    func getImageData(forLatitude latitude: Double, andLongitude longitude: Double, withRadius radius: Double, completionHandlerForImageData: @escaping (_ imageData: [NSData]?, _ errorMessage: String?) -> Void) {
+    func getImageUrls(forLatitude latitude: Double, andLongitude longitude: Double, withRadius radius: Double, completionHandlerForImageUrls: @escaping (_ imageUrls: [URL]?, _ errorMessage: String?) -> Void) {
         
         // Set the parameters
         let parameters: [String:Any] = [
@@ -37,7 +37,7 @@ class FlickrClient {
         
         // Get the Flickr URL
         guard let url = createFlickrUrl(fromParameters: parameters) else {
-            completionHandlerForImageData(nil, "Couldn't create Flickr URL")
+            completionHandlerForImageUrls(nil, "Couldn't create Flickr URL")
             return
         }
         
@@ -48,20 +48,20 @@ class FlickrClient {
             
             // Check if there was an error
             guard error == nil else {
-                completionHandlerForImageData(nil, "Error: \(error!.localizedDescription)")
+                completionHandlerForImageUrls(nil, "Error: \(error!.localizedDescription)")
                 return
             }
             
             // Check if the status code implies a successful response
             guard let statusCode = (response as? HTTPURLResponse)?.statusCode,
                 statusCode >= 200 && statusCode <= 299 else {
-                    completionHandlerForImageData(nil, "Received unsuccessful status code")
+                    completionHandlerForImageUrls(nil, "Received unsuccessful status code")
                     return
             }
             
             // Check if data was received
             guard let data = data else {
-                completionHandlerForImageData(nil, "No data received")
+                completionHandlerForImageUrls(nil, "No data received")
                 return
             }
             
@@ -70,34 +70,63 @@ class FlickrClient {
             do {
                 jsonData = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String:Any]
             } catch {
-                completionHandlerForImageData(nil, "JSON deserialization error: \(error.localizedDescription)")
+                completionHandlerForImageUrls(nil, "JSON deserialization error: \(error.localizedDescription)")
                 return
             }
             
             // Get the array of photo dictionaries by extracting them from the JSON object
             guard let photos = jsonData[FlickrConstant.JSONResponseKey.photos] as? [String:Any],
-            let photoArray = photos[FlickrConstant.JSONResponseKey.photoArray] as? [[String:Any]] else {
-                    completionHandlerForImageData(nil, "Error when parsing JSON")
+                let photoArray = photos[FlickrConstant.JSONResponseKey.photoArray] as? [[String:Any]] else {
+                    completionHandlerForImageUrls(nil, "Error when parsing JSON")
                     return
             }
             
             // Create an empty array of NSData objects and fill it by iterating over all the received image dictionaries
             // and using the images' URL string to create NSData objects from URLs
-            var imageData = [NSData]()
+            var imageUrls = [URL]()
             for photo in photoArray {
                 guard let currentImageUrlString = photo[FlickrConstant.JSONResponseKey.imageMediumUrl] as? String,
-                    let currentImageUrl = URL(string: currentImageUrlString),
-                    let currentImageData = NSData(contentsOf: currentImageUrl) else {
-                        completionHandlerForImageData(nil, "Couldn't create data for image")
+                    let currentImageUrl = URL(string: currentImageUrlString) else {
+                        completionHandlerForImageUrls(nil, "Couldn't create URLs")
                         return
                 }
                 
-                imageData.append(currentImageData)
+                imageUrls.append(currentImageUrl)
                 
             }
             
             // Call the completion handler and pass it the image data
-            completionHandlerForImageData(imageData, nil)
+            completionHandlerForImageUrls(imageUrls, nil)
+            
+        }
+        
+        task.resume()
+        
+    }
+    
+    func downloadImageData(fromUrl url: URL, completionHandlerForImageData: @escaping (_ data: NSData?, _ errorMessage: String?) -> Void) {
+        // Create the request
+        let request = URLRequest(url: url)
+        
+        // Make the request
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard error == nil else {
+                completionHandlerForImageData(nil, error!.localizedDescription)
+                return
+            }
+            
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode,
+                statusCode >= 200 && statusCode <= 299 else {
+                    completionHandlerForImageData(nil, "Unsuccessful status code \((response as? HTTPURLResponse)?.statusCode)")
+                    return
+            }
+            
+            guard let data = data else {
+                completionHandlerForImageData(nil, "Didn't receive data")
+                return
+            }
+            
+            completionHandlerForImageData(data as NSData, nil)
             
         }
         
