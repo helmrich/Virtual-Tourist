f420bd480cbbce4c2f4da077d1b6a51146212893
Tobias Helmrich <me@tobias-helmrich.de>
2016-10-29 15:45:11 +0200

    Create custom collection view cell for images

---
+++ Virtual Tourist/ImageCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCollectionViewCell.swift
+//  Virtual Tourist
+//
+//  Created by Tobias Helmrich on 27.10.16.
+//  Copyright © 2016 Tobias Helmrich. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
+}
