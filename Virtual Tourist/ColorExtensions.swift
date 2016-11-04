d1b31113b419ecfec28ec2d4dfcf41f54d17a5f6
Tobias Helmrich <me@tobias-helmrich.de>
2016-11-05 00:22:02 +0100

    Add extension for UIColor that adds the "default " red of a cancel button as a type property

---
+++ Virtual Tourist/ColorExtensions.swift
@@ -0,0 +1,13 @@
+//
+//  ColorExtensions.swift
+//  Virtual Tourist
+//
+//  Created by Tobias Helmrich on 02.11.16.
+//  Copyright © 2016 Tobias Helmrich. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let cancelRed = UIColor(red: 255, green: 59 / 255, blue: 48 / 255, alpha: 1)
+}
